9601c54f23e7ae88a8072cd68239cb9b1b477a93
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-07-11 19:57:26 -0400

    changed prices to dollar amounts

--- models/transactions.js
+++ models/transactions.js
@@ -2,7 +2,7 @@ module.exports = [
     {
       id: 0,
       item_name: "Rent",
-      amount: 2400,
+      amount: 2400.00,
       date: "07/09/2023",
       from: "landlord",
       category: "income"
@@ -10,7 +10,7 @@ module.exports = [
     {
       id: 1,
       item_name: "Tuition",
-      amount: 1700,
+      amount: 1700.00,
       date: "07/05/2023",
       from: "School",
       category: "Savings"
@@ -18,7 +18,7 @@ module.exports = [
     {
       id: 2,
       item_name: "Saturday night",
-      amount: 250,
+      amount: 250.00,
       date: "07/08/2023",
       from: "Jenny",
       category: "Entertainment"
@@ -26,7 +26,7 @@ module.exports = [
     {
       id: 3,
       item_name: "Groceries",
-      amount: 150,
+      amount: 150.00,
       date: "07/10/2023",
       from: "Supermarket",
       category: "Expenses"
@@ -34,7 +34,7 @@ module.exports = [
     {
       id: 4,
       item_name: "Dinner with friends",
-      amount: 80,
+      amount: 80.00,
       date: "07/07/2023",
       from: "Restaurant",
       category: "Entertainment"
@@ -42,7 +42,7 @@ module.exports = [
     {
       id: 5,
       item_name: "Gas bill",
-      amount: 50,
+      amount: 50.00,
       date: "07/06/2023",
       from: "Utility company",
       category: "Bills"
