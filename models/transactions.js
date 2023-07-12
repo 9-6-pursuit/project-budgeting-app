7aaf6c5d4d905af483778d66eb057c2f90ceca0e
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-07-11 22:46:57 -0400

    made changes to models so function would work

--- models/transactions.js
+++ models/transactions.js
@@ -1,43 +1,43 @@
 module.exports = [
     {
       id: 0,
-      item_name: "Rent",
-      amount: 2400.00,
+      item_name: "Yankee Tickets",
+      amount: 45.00,
       date: "07/09/2023",
-      from: "landlord",
-      category: "income"
-    },
-    {
-      id: 1,
-      item_name: "Tuition",
-      amount: 1700.00,
-      date: "07/05/2023",
-      from: "School",
-      category: "Savings"
-    },
-    {
-      id: 2,
-      item_name: "Saturday night",
-      amount: 250.00,
-      date: "07/08/2023",
-      from: "Jenny",
+      from: "StubHub",
       category: "Entertainment"
     },
     {
-      id: 3,
-      item_name: "Groceries",
+      id: 1,
+      item_name: "Massar Capital Management",
+      amount: 570.00,
+      date: "07/05/2023",
+      from: "Employer",
+      category: "Deposit"
+    },
+    {
+      id: 2,
+      item_name: "Phone Bill",
       amount: 150.00,
+      date: "07/08/2023",
+      from: "at&t",
+      category: "Bills"
+    },
+    {
+      id: 3,
+      item_name: "Car Insurance",
+      amount: 125.00,
       date: "07/10/2023",
-      from: "Supermarket",
+      from: "Geico",
       category: "Expenses"
     },
     {
       id: 4,
-      item_name: "Dinner with friends",
+      item_name: "John Zelle",
       amount: 80.00,
       date: "07/07/2023",
-      from: "Restaurant",
-      category: "Entertainment"
+      from: "John Apple",
+      category: "Deposit"
     },
     {
       id: 5,
